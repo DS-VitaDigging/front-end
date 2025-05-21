6fef275abdc8d7952ff504768d56673f970949c3
soyyyyy <da5868@naver.com>
2025-05-22 00:47:32 +0900

    setting: 초기세팅

---
+++ vita-digging/src/styles/theme.js
@@ -0,0 +1,30 @@
+const theme = {
+  colors: {
+    // 텍스트 색상
+    'text-primary': '#020202',
+    'text-sub1': '#4C4C4C',
+    'text-sub2': '#6E6E6E',
+    'text-sub3': '#AFAFAF',
+    'text-white': '#FFFFFF',
+
+    // 배경 색상
+    'bg-black': '#000000',
+    'bg-gray1': '#E8E8E8',
+    'bg-gray2': '#F8F8F8',
+    'bg-white': '#FFFFFF',
+    'bg-grayscale': 'rgba(0, 0, 0, 0.6)',
+    
+    // System Colors
+    'system-press': '#0077FF',
+    'system-error': '#FF0000',
+    'system-success': '#089900',
+    
+    // Main Colors
+    'color-primary': '#626F47',
+    'color-sub1': '#A4B465',
+    'color-sub2': '#FFCF50',
+    'color-point1': '#FEFAE0',
+  },
+};
+
+export default theme;

7f6297d37e92658235ad6d88aebe83dcb0eb8596
bboyo <borybop.kim@gmail.com>
2025-07-01 18:02:10 +0900

    feat: 로그인페이지 작업 완료

--- vita-digging/src/router.js
+++ vita-digging/src/router.js
@@ -1,10 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
+<<<<<<< Updated upstream
 import Home from './pages/Home';
+=======
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import Signup from './pages/Signup/Signup';
+>>>>>>> Stashed changes
 
 export const AppRouter = () => {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
         </Routes>
     );
 };

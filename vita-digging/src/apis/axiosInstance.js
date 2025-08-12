6219943efbd2a1b427e855ab37af566331de1840
Soy <da5868@naver.com>
2025-08-13 02:53:22 +0900

    chore: 서버 baseURL 경로 수정

--- vita-digging/src/apis/axiosInstance.js
+++ vita-digging/src/apis/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.SERVER_API_BASE_URL,
+  baseURL: process.env.REACT_APP_SERVER_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },

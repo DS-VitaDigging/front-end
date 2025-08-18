5a685bd0f8d5712708523ae0cadf31fae441713e
Soy <da5868@naver.com>
2025-08-19 03:00:11 +0900

    feat: 토큰 세션 만료시 로그아웃 처리

--- vita-digging/src/apis/axiosInstance.js
+++ vita-digging/src/apis/axiosInstance.js
@@ -1,20 +1,32 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_SERVER_API_BASE_URL,
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  withCredentials: true,
+    baseURL: 'http://localhost:8080',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    withCredentials: true,
 });
 
 axiosInstance.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
+    (response) => {
+        return response;
+    },
+    (error) => {
+        // 401 Unauthorized 시 세션 만료 처리
+        if (error.response && error.response.status === 401) {
+            const token = localStorage.getItem('accessToken');
+            
+            // 토큰이 있는데 401이면 세션 만료
+            if (token) {
+                alert('세션이 만료되었습니다. 다시 로그인해주세요.');
+                localStorage.removeItem('accessToken');
+                window.location.reload(); 
+            }
+        }
+        
+        return Promise.reject(error);
+    }
 );
 
 export default axiosInstance;
